a6d8ef19d9fe0eb9ba416b7a11df0b741d4b1980
Marcel Sziener <m.sziener@gmail.com>
2021-08-20 22:42:18 +0200

    .

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
-// var indexRouter = require("./routes/index");
+var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const eventsRouter = require("./routes/events");
 
